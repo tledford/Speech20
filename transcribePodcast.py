f47a5577961359da4bb6bb8dd99961ae55ec0c17
Mikaami <asoriano@csumb.edu>
2015-03-25 14:17:49 -0700

    Added comments

--- transcribePodcast.py
+++ transcribePodcast.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pylab
 
 system = platform.system()
-
+#used to determine which operating system the program is beign runned on
 def open_file():
     if system == "Darwin":
         os.system("open newfile.txt")
@@ -15,28 +15,28 @@ def open_file():
         os.system("gedit newfile.txt")
 
 def transcribe():
-	origAudio = wave.open('fourscore.wav','r')
-	numFrames = origAudio.getnframes()
-	sampWidth = origAudio.getsampwidth()
-	frameRate = origAudio.getframerate()
-	length = numFrames / float(frameRate)
-	nChannels = origAudio.getnchannels()
-	file = open("newfile.txt", "w")
+	origAudio = wave.open('fourscore.wav','r') #this is the podcast we are transcribing
+	numFrames = origAudio.getnframes()         #This gets the framerates in other words how fast the audio is.
+	sampWidth = origAudio.getsampwidth()#All of this has to do with the audio file
+	frameRate = origAudio.getframerate()#How fast it goes through all the frames
+	length = numFrames / float(frameRate) #I grabed the amount of frames divided by the framerate to get the length of the file.
+	nChannels = origAudio.getnchannels()#Checks if the original is mono audio or multi channel.
+	file = open("newfile.txt", "w")#opens the file to output the text.
 
 
-	start_pos = float(0)
+	start_pos = float(0) #this is how we handle the loops with a start position and end position.
 	end_pos = float(6)
 
 	while end_pos <= length: #only 7 to test short clip, real version should be 'length'
 
-		origAudio.setpos(start_pos * frameRate)
-		chunkData = origAudio.readframes(int((end_pos - start_pos) * frameRate))
+		origAudio.setpos(start_pos * frameRate) #We grab the start position multiplied by the framerate to know where to start.
+		chunkData = origAudio.readframes(int((end_pos - start_pos) * frameRate))#we do the same for the end position to know where is the last frame.
 
 
-		chunkAudio = wave.open('new_file.wav', 'w')
-		chunkAudio.setsampwidth(sampWidth)
-		chunkAudio.setnchannels(1)
-		chunkAudio.setframerate(frameRate)
+		chunkAudio = wave.open('new_file.wav', 'w') #We create a new wavfile to pass in a 5 second wave file
+		chunkAudio.setsampwidth(sampWidth)          #The api can only handle a 6 second wav file
+		chunkAudio.setnchannels(1)                  #we make the channels and framerates and frames the same as the original.
+		chunkAudio.setframerate(frameRate)          #this way each time it passes in 6 seconds of audio.
 		chunkAudio.writeframes(chunkData)
 		chunkAudio.close()
 
@@ -53,11 +53,11 @@ def transcribe():
 		
 		except LookupError:                                 # speech is unintelligible
 			start_pos -= 6.25
-			end_pos -= 6.25
+			end_pos -= 6.25                             #it will try the same points with a little bit less audio.
 		
 	
-		end_pos += 6
-		start_pos += 6
+		end_pos += 6                #if it was succesful in transcribing the audio then it will increase the start position for the next part of the audio file.
+		start_pos += 6              #we also increase the endposition to a new time.
 	file.close()
 	open_file()
 
